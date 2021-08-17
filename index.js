4fcc8771594d87eeb5428bc6b4bcac9a4b6b6cc1
klakegg <klakegg@users.noreply.github.com>
2021-08-17 10:49:27 +0000

    deploy: d7b781eb395d44b5c0884db8cde0e75414265556

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ async function run() {
         if (['number', 'boolean', 'string'].indexOf(typeof entry[1]) >= 0)
             core.setOutput(entry[0], entry[1]);
     });
+    if (/^v[0-9]/.test(release.data.tag_name))
+        core.setOutput('version', release.data.tag_name.substr(1));
 }
 run();
