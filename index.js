01abfae65ad74f5af67f142559b01fda1ea34d3e
klakegg <klakegg@users.noreply.github.com>
2021-08-21 13:11:45 +0000

    deploy: e1a9c30424b0233ae2b4ab3505b3225735c7dc33

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = require("@actions/core");
+const github = require("@actions/github");
+async function run() {
+    var token = core.getInput('token', { required: true });
+    var octokit = github.getOctokit(token);
+    var owner = core.getInput('owner', { required: true });
+    var repo = core.getInput('repo', { required: true });
+    var release;
+    try {
+        release = await octokit.rest.repos.getLatestRelease({ owner, repo });
+    }
+    catch (e) {
+        core.setFailed(`Received ${e.status} during lookup.`);
+        return;
+    }
+    Object.entries(release.data).forEach(entry => {
+        if (['number', 'boolean', 'string'].indexOf(typeof entry[1]) >= 0)
+            core.setOutput(entry[0], entry[1]);
+    });
+    if (/^[vV]\d/.test(release.data.tag_name))
+        core.setOutput('version', release.data.tag_name.substr(1));
+}
+run();
