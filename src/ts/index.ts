8a65c718e394178995aebdd4085cf26cee19c102
Erlend Klakegg Bergheim <erlend@klakegg.net>
2021-11-02 07:10:25 +0100

    Making owner optional

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,23 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+const repo_matcher = new RegExp('^(.+)\/(.+)$')
+
 async function run() {
     // Fetch token and create client
-    var token = core.getInput('token')
+    var token = core.getInput('token', { required: true })
     var octokit = github.getOctokit(token)
 
     // Fetch owner and repo
-    var owner = core.getInput('owner', {required: true})
-    var repo = core.getInput('repo', {required: true})
+    var repo = core.getInput('repo', { required: true })
+
+    var owner
+    if (repo.match(repo_matcher)) {
+        [, owner, repo] = repo.match(repo_matcher)
+    } else {
+        owner = core.getInput('owner', { required: true })
+    }
+
     var tag = core.getInput('tag')
 
     // Fetch latest release
@@ -16,9 +25,9 @@ async function run() {
 
     try {
         if (tag == "")
-            release = await octokit.rest.repos.getLatestRelease({owner, repo})
+            release = await octokit.rest.repos.getLatestRelease({ owner, repo })
         else
-            release = await octokit.rest.repos.getReleaseByTag({owner, repo, tag})
+            release = await octokit.rest.repos.getReleaseByTag({ owner, repo, tag })
     } catch (e) {
         core.setFailed(`Received ${e.status} during lookup of ${e.response.url}.`)
         return
